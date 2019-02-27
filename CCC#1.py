0873f11426b3bd3c0737a78930f528543e09780b
Ivan Lee <ivan.lee@ucc.on.ca>
2019-02-27 11:40:51 -0500

    create

---
+++ CCC#1.py
@@ -0,0 +1,14 @@
+#1. get the input
+
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+num4 = int(input())
+
+#2. processing
+
+if ((num1 == 8 or num1 == 9) and (num2 == num3) and (num4 == 8 or num4 == 9)):
+    #3 output
+    print("ignore")
+else:
+    print("answer")
